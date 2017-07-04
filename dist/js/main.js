e651ad5c8c854dee1f13390d51489073f54ad161
user <user@Mac-mini-user.local>
2017-07-04 18:40:48 +0300

    first

---
+++ dist/js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+$(document).ready(function () {
+
+  var scene = document.getElementById('scene');
+  var parallax = new Parallax(scene);
+});
---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+  let scene = document.getElementById('scene');
+  let parallax = new Parallax(scene);
+
+})
